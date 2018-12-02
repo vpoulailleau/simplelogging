0b87579f24e2613bd1afa748ca2d878b8ba7b9c2
Vincent Poulailleau <vpoulailleau@gmail.com>
2018-12-02 20:40:13 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vpoulailleau/simplelogging",
-    version="version='0.2.0'",
+    version="version='0.3.0'",
     zip_safe=False,
 )
--- simplelogging/__init__.py
+++ simplelogging/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Vincent Poulailleau"""
 __email__ = "vpoulailleau@gmail.com"
-__version__ = "__version__ = '0.2.0'"
+__version__ = "__version__ = '0.3.0'"
 
 import inspect
 import logging
