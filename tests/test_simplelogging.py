96e0b60c65dbd7a52f7f9cd922a01e8d4e15dfa1
Vincent Poulailleau <vpoulailleau@gmail.com>
2018-12-09 18:01:43 +0100

    test console

--- tests/test_simplelogging.py
+++ tests/test_simplelogging.py
@@ -41,3 +41,55 @@ def test_default_logger_console():
             break
     else:
         assert 0, "No console handler found"
+
+
+def test_default_logger_console_formatter():
+    """Test default logger level stream formatter presence"""
+    main_log = simplelogging.get_logger("__main__")
+    assert main_log.handlers
+    for handler in main_log.handlers:
+        if isinstance(handler, colorlog.StreamHandler):
+            if isinstance(handler.formatter, colorlog.ColoredFormatter):
+                break
+    else:
+        assert 0, "No good console formatter found"
+
+
+def test_default_logger_console_formatter_parameters():
+    """Test default logger level stream formatter parameters"""
+    main_log = simplelogging.get_logger("__main__")
+    log_colors = {
+        "DEBUG": "blue",
+        "INFO": "black,bg_green",
+        "WARNING": "black,bg_yellow",
+        "ERROR": "white,bg_red",
+        "CRITICAL": "red,bg_white",
+    }
+
+    secondary_log_colors = {}
+
+    assert main_log.handlers
+    for handler in main_log.handlers:
+        if isinstance(handler, colorlog.StreamHandler):
+            if isinstance(handler.formatter, colorlog.ColoredFormatter):
+                formatter = handler.formatter
+                assert formatter._fmt == simplelogging.DEFAULT_CONSOLE_FORMAT
+                assert formatter.datefmt is None
+                assert formatter.log_colors == log_colors
+                assert formatter.secondary_log_colors == secondary_log_colors
+                assert isinstance(formatter._style, logging._STYLES["%"][0])
+                break
+    else:
+        assert 0, "No good console formatter found"
+
+
+def test_default_logger_console_level():
+    """Test default logger level stream formatter presence"""
+    main_log = simplelogging.get_logger("__main__")
+    assert main_log.handlers
+    for handler in main_log.handlers:
+        if isinstance(handler, colorlog.StreamHandler):
+            if handler.level == simplelogging.INFO:
+                break
+    else:
+        assert 0, "Not good console level"
